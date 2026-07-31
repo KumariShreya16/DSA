7171a06c1a8b57ef6e2f92cfde66c3ce8bc6a101
KumariShreya16 <kshreyya112@gmail.com>
2026-07-31 18:11:37 +0530

    Time: 0 ms (100%), Space: 42.8 MB (95.65%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,8 +1,10 @@
 class Solution {
     public int minimumPushes(String word) {
         int n = word.length();
-        int blocks = n / 8;
-        return (blocks * (blocks + 1) * 4)
-                + (n % 8) * (blocks + 1);
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=i/8+1;
+        }
+        return sum;
     }
 }
