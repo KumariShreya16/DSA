9a5e651f81529dbaa539b5f1f121cc5611eedaa7
KumariShreya16 <kshreyya112@gmail.com>
2026-08-10 15:21:08 +0530

    Time: 1 ms (100%), Space: 48.5 MB (94.56%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,26 +1,51 @@
 class Solution {
-    public double findMedianSortedArrays(int[] a, int[] b) {
-        if (a.length > b.length) return findMedianSortedArrays(b, a);
 
-        int m = a.length, n = b.length, l = 0, r = m;
-
-        while (l <= r) {
-            int i = (l + r) / 2;
-            int j = (m + n + 1) / 2 - i;
-
-            int al = i == 0 ? Integer.MIN_VALUE : a[i - 1];
-            int ar = i == m ? Integer.MAX_VALUE : a[i];
-            int bl = j == 0 ? Integer.MIN_VALUE : b[j - 1];
-            int br = j == n ? Integer.MAX_VALUE : b[j];
-
-            if (al <= br && bl <= ar)
-                return (m + n) % 2 == 1
-                    ? Math.max(al, bl)
-                    : (Math.max(al, bl) + Math.min(ar, br)) / 2.0;
-
-            if (al > br) r = i - 1;
-            else l = i + 1;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Always binary search on the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-        return 0;
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int left = 0;
+        int right = m;
+
+        while (left <= right) {
+            int partition1 = (left + right) / 2;
+            int partition2 = (m + n + 1) / 2 - partition1;
+
+            int left1 = (partition1 == 0)  ? Integer.MIN_VALUE   : nums1[partition1 - 1];
+            int right1 = (partition1 == m)  ? Integer.MAX_VALUE  : nums1[partition1];
+            int left2 = (partition2 == 0)  ? Integer.MIN_VALUE  : nums2[partition2 - 1];
+            int right2 = (partition2 == n)  ? Integer.MAX_VALUE  : nums2[partition2];
+
+            if (left1 <= right2 && left2 <= right1) {
+
+                // Odd total length
+                if ((m + n) % 2 == 1) {
+                    return Math.max(left1, left2);
+                }
+
+                // Even total length
+                int maxLeft = Math.max(left1, left2);
+                int minRight = Math.min(right1, right2);
+
+                return ((double) maxLeft + minRight) / 2.0;
+            }
+
+            // partition1 is too far right
+            else if (left1 > right2) {
+                right = partition1 - 1;
+            }
+
+            // partition1 is too far left
+            else {
+                left = partition1 + 1;
+            }
+        }
+
+        return 0.0;
     }
 }
