1592962824c2a74a396c96c0927a11645441f34a
KumariShreya16 <kshreyya112@gmail.com>
2026-08-11 23:56:47 +0530

    Time: 4 ms (100%), Space: 46.1 MB (13.19%) - LeetHub

---
+++ 2739-total-distance-traveled/2739-total-distance-traveled.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int distanceTraveled(int mainTank, int additionalTank) {
+        return (mainTank+Math.min((mainTank - 1) / 4, additionalTank)) * 10;
+    }
+}
+   
+      
+    
