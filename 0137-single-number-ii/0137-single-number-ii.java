85078071286bbe678819cbc83624752660398974
KumariShreya16 <kshreyya112@gmail.com>
2026-09-11 21:22:28 +0530

    Time: 0 ms (100%), Space: 45.3 MB (51.03%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int one=0, two=0;
-        for(final int num:nums){
+        for( int num:nums){
             one^=(num & ~two);
             two^=(num & ~one);
         }
